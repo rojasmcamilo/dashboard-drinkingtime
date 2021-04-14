6707cf644bcf1ebd25cbe49208bb8e935cb49ede
rojasmcamilo <cristhianrojas.95@live.com>
2021-04-14 17:57:43 -0500

    prueba

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer class="sticky-footer bg-white">
+				<div class="container my-auto">
+					<div class="copyright text-center my-auto">
+						<span>Copyright &copy; Dashboard 2020</span>
+					</div>
+				</div>
+		</footer>
+    );
+}
+export default Footer;
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,59 +1,52 @@
 import React from "react";
 
-const SideBar = () => {
-  return (
-    <>
-      <ul
-        className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion"
-        id="accordionSidebar"
-      >
-        <a
-          className="sidebar-brand d-flex align-items-center justify-content-center"
-          href="/"
-        >
-          <div className="sidebar-brand-icon">
-            <i className="fas fa-chart-line"></i>
-          </div>
-          <div className="sidebar-brand-text mx-3">Admin</div>
-        </a>
+function SideBar (){
 
-        <hr className="sidebar-divider my-0" />
+    return (
+        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-        <li className="nav-item active">
-          <a className="nav-link" href="/">
-            <i className="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span>
-          </a>
-        </li>
+            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+              <div className="sidebar-brand-icon">
+                <i className="fas fa-chart-line"></i>
+              </div>
+              <div className="sidebar-brand-text mx-3">Admin</div>
+            </a>
 
-        <hr className="sidebar-divider" />
+            <hr className="sidebar-divider my-0"/>
 
-        <div className="sidebar-heading">Actions</div>
+            <li className="nav-item active">
+              <a className="nav-link" href="/">
+                <i className="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span></a>
+            </li>
 
-        <li className="nav-item">
-          <a className="nav-link collapsed" href="/">
-            <i className="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-          </a>
-        </li>
+            <hr className="sidebar-divider"/>
 
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            <i className="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span>
-          </a>
-        </li>
+            <div className="sidebar-heading">Actions</div>
 
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            <i className="fas fa-fw fa-table"></i>
-            <span>Tables</span>
-          </a>
-        </li>
+            <li className="nav-item">
+              <a className="nav-link collapsed" href="/">
+                <i className="fas fa-fw fa-folder"></i>
+                <span>Pages</span>
+              </a>
+            </li>
 
-        <hr className="sidebar-divider d-none d-md-block" />
-      </ul>
-    </>
-  );
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <i className="fas fa-fw fa-chart-area"></i>
+                <span>Charts</span></a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                <i className="fas fa-fw fa-table"></i>
+                <span>Tables</span></a>
+            </li>
+
+            <hr className="sidebar-divider d-none d-md-block"/>
+          </ul>
+    )
+
 };
+
 export default SideBar;
